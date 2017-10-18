551ce19f0ca4f2deb59f820ba7d3e020c42cdb9d
Antoine Mineau <amineau@student.42.fr>
2017-10-18 17:13:05 +0200

    check parenthese

--- expert_system/rules.py
+++ expert_system/rules.py
@@ -3,21 +3,29 @@ import re
 class Rules:
     _rules = list()
     def __init__(self):
-		pass
+        pass
 
-	def __str__(self):
-		return str(self._rules)
-	
-	def _check_parenthesis(self, line):
-		return True
+    def __str__(self):
+        return str(self._rules)
+    
+    def _check_parentheses(self, line):
+        i = 0
+        for c in line:
+            if c == '(':
+                i += 1
+            elif c == ')'
+                i -= 1
+            if i == -1
+                return False
+        return i == 0
 
-	def push(self, line):
-		if re.match("^\(?!?[A-Z]\)?(([+^|]\(?!?[A-Z]\)?)?)+(<?=>)\(?!?[A-Z]\)?(([+^|]\(?!?[A-Z]\)?)?)+$", line) is None
-			or _check_parenthesis(line) is False:
-			print("Error during parse rules")
-			sys.exit(1)
-		if _check_parenthesis(line) is False
-		self._rules.append(line)
+    def push(self, line):
+        if re.match("^\(?!?[A-Z]\)?(([+^|]\(?!?[A-Z]\)?)?)+(<?=>)\(?!?[A-Z]\)?(([+^|]\(?!?[A-Z]\)?)?)+$", line) is None
+            or _check_parentheses(line) is False:
+            print("Error during parse rules")
+            sys.exit(1)
+        self._rules.append(line)
 
-	def getRules(self):
-		return self._rules
+    def getRules(self):
+        return self._rules
+
