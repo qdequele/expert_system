b4117b2ce21b8e193d84a686359f71d0a2dc6471
qdequele <quentin@dequelen.me>
2017-10-18 14:17:27 +0200

    update file

--- expert_system/__main__.py
+++ expert_system/__main__.py
@@ -1,5 +1,6 @@
 import sys
-import rules
+import fact
+from file import *
 
 def check_params():
 	if len(sys.argv) > 2:
@@ -9,30 +10,6 @@ def check_params():
 		print("Please enter a file as argument")
 		sys.exit(1)
 
-def parse_file():
-	try:
-		file = open(sys.argv[1])
-	except IOError:
-		print("Open file failed")
-		sys.exit(1)
-	text = file.readlines()
-	for i, _ in enumerate(text):
-		text[i] = text[i].translate(None, '\t\n')
-		text[i], _, _ = text[i].partition('#')
-		text[i] = text[i].strip()
-		if len(text[i]) == 0:
-			text[i] = None
-		pass
-	text = filter(None, text)
-	return text
-
-def get_values(text):
-	print(text)
-	questions = filter(lambda x: x.startswith('?'), text)
-	print(questions)
-	affirmations = filter(lambda x: x.startswith('='), text)
-	print(affirmations)
-
 check_params()
-text = parse_file()
-get_values(text)
+file = File(sys.argv[1])
+print(file)
